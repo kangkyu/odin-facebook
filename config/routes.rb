b63bfe445e86eb8230d8e8377c5272ed69697c98
Thomas Jung <thomasjunginbox@gmail.com>
2015-07-05 15:18:11 -0700

    Basic user model through Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'static_pages#home'
+  # devise_scope :user do
+  #   root to: 'devise/sessions#new'
+  # end
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -8,7 +15,7 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
+  # Example of named route that can be invoked with purchase_url(id: product.id)  
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
